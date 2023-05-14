19c3d0dadcc4e15a27a6f4e7692006f970520144
Alvaro Ramirez <ard00032@red.ujaen.es>
2023-05-14 21:00:05 +0200

    Añadir MS Judo + Config Frontend

--- front-end/static-files/js/front-end.js
+++ front-end/static-files/js/front-end.js
@@ -8,8 +8,13 @@
 /// Espacio de nombres
 let Frontend = {};
 
+
 /// Dirección del MS que funciona como API_GATEWAY
 Frontend.API_GATEWAY = "http://localhost:8001"
+
+/// Algunas constantes relacionadas con CSS y HTML
+Frontend.CLASS_MOSTRAR = "mostrar"
+Frontend.CLASS_OCULTAR = "ocultar"
 
 /// Algunas constantes relacionadas con CSS y HTML
 Frontend.ID_SECCION_PRINCIPAL = "seccion-principal"
@@ -19,6 +24,44 @@ Frontend.ID_SECCION_PRINCIPAL_CONTENIDO = "seccion-principal-contenido"
 /// Objeto Article dentro Frontend para tratar con el contenido del elemento Article del DOM
 Frontend.Article = {}
 
+Frontend.mostrarAcercaDeTodos = function(){
+    Frontend.AcercaDeMensaje="";
+    Judo.descargarRuta("/judo/acercade", this.mostrarAcercaDe)
+}
+
+Frontend.mostrarAcercaDe = function(datosDescargados){
+    datosDescargados = datosDescargados || this.datosDescargadosNulos
+    // Si datos descargados NO es un objeto 
+    if (typeof datosDescargados !== "object") datosDescargados = this.datosDescargadosNulos
+    // Si datos descargados NO contiene los campos mensaje, autor, o email
+    if (typeof datosDescargados.mensaje === "undefined" ||
+        typeof datosDescargados.autor === "undefined" ||
+        typeof datosDescargados.email === "undefined" ||
+        typeof datosDescargados.fecha === "undefined"
+    ) datosDescargados = this.datosDescargadosNulos
+    const mensajeAMostrar = `<div>
+    <p>${datosDescargados.mensaje}</p>
+    <ul>
+        <li><b>Autor/a</b>: ${datosDescargados.autor}</li>
+        <li><b>E-mail</b>: ${datosDescargados.email}</li>
+        <li><b>Fecha</b>: ${datosDescargados.fecha}</li>
+    </ul>
+    </div>
+    `;
+   Frontend.AcercaDeMsj += mensajeAMostrar;
+   Frontend.Article.actualizar("Acerca De", Frontend.AcercaDeMsj);
+}
+
+/**
+ * Muestro el article
+ * @returns El propio Article para poder concatenar llamadas
+ */
+Frontend.Article.mostrar = function () {
+    let article = document.getElementById(Frontend.ID_SECCION_PRINCIPAL);
+    Frontend.quitarClase(Frontend.ID_SECCION_PRINCIPAL, Frontend.CLASS_OCULTAR)
+        .aniadirClase(Frontend.ID_SECCION_PRINCIPAL, Frontend.CLASS_MOSTRAR)
+
+}
 
 /**
  * Cambia toda la información del article
